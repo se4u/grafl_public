f8d0cb114d8df6ae8956c0929d5c9b32a2804628
Pushpendre Rastogi <pushpendre@gmail.com>
2015-08-19 03:15:47 -0400

    Add more default search paths to config

--- src/config.py
+++ src/config.py
@@ -3,25 +3,33 @@
 | Description : The module that stores the command line arguments and other miscellaneous global parameters.
 | Author      : Pushpendre Rastogi
 | Created     : Mon Aug 17 23:37:56 2015 (-0400)
-| Last-Updated: Tue Aug 18 16:16:49 2015 (-0400)
+| Last-Updated: Tue Aug 18 20:12:36 2015 (-0400)
 |           By: Pushpendre Rastogi
-|     Update #: 9
+|     Update #: 17
 '''
 args = None
 from functools import wraps
 project_dir = r'~/Dropbox/paper/grafl/'
 project_pathprefix = ['',
-                      project_dir+'src/experiments/']
+                      project_dir,
+                      project_dir+'src/experiments/',
+                      project_dir+'res/']
 import __builtin__
+builtin_open = __builtin__.open
 import os
 def open(name, mode='rb', buffering=1024):
     for prefix in project_pathprefix:
         try:
             path = os.path.expanduser(os.path.join(prefix, name))
-            print "Opening path ", str(path)
-            return __builtin__.open(path,
-                                    mode=mode,
-                                    buffering=buffering)
+            f = builtin_open(path, mode=mode, buffering=buffering)
+            if (path.startswith(project_dir)
+                or 'pylearn2' in path
+                or 'Lasagne' in path
+                or 'theano' in path
+                or path[0] != r'/'):
+                print "Opened file ", str(path)
+            return f
         except IOError:
             pass
     raise IOError("Couldn't open %s"%name)
+__builtin__.open = open
